60b11f014d3c86a70a02e7a134b4712afb98a1d0
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2012-02-26 22:54:23 +0900

    Init Sandbox/CollisionDetector.cs. NEED to be replaced.

---
+++ Unicoen.Apps/RefactoringDSL/Sandbox/CollisionDetector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unicoen.Model;
+
+namespace Unicoen.Apps.RefactoringDSL.Sandbox {
+	public class CollisionDetector {
+		public static UnifiedElement DetectCollision(UnifiedElement src, UnifiedElement target) {
+			foreach (var element in src.Descendants()) {
+				if (element.GetType() != target.GetType()) {
+					continue;
+				}
+				// 対象の型によって下に委譲
+				if (target is UnifiedVariableDefinition) {
+					return DetectVariableCollision(element as UnifiedVariableDefinition, target as UnifiedVariableDefinition);
+				}
+
+				if (target is UnifiedFunctionDefinition) {
+					return DetectFunctionCollision(element as UnifiedFunctionDefinition, target as UnifiedFunctionDefinition);
+				}
+
+			}
+
+			throw  new NotSupportedException("さぽーとしてないお(´・ω・｀)");
+
+		}
+
+		// src と dst の衝突を調べて，衝突していれば src を，そうでなければ null を返す
+		private static UnifiedVariableDefinition DetectVariableCollision(UnifiedVariableDefinition src, UnifiedVariableDefinition dst) {
+			var judges = new List<Boolean>();
+			judges.Add(src.Name.Name == dst.Name.Name);
+
+			if (judges.All(e => true)) {
+				return null;
+			}
+			return src;
+		}
+
+		private static UnifiedFunctionDefinition DetectFunctionCollision(UnifiedFunctionDefinition src, UnifiedFunctionDefinition dst) {
+			// Judge whether src and dst have same signature
+			var judges = new List<Boolean>();
+			judges.Add(src.Name.Name == dst.Name.Name);
+			judges.Add(src.Parameters.All(e => dst.Parameters.Any(d => d.Equals(e))));
+
+			if (judges.All(e => true)) {
+				return null;
+			}
+			return src;
+		}
+
+	}
+}
---
+++ Unicoen.Apps/RefactoringDSL/Sandbox/CollisionDetectorTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Unicoen.Languages.Java;
+using Unicoen.Model;
+using Unicoen.Tests;
+
+namespace Unicoen.Apps.RefactoringDSL.Sandbox {
+	public class CollisionDetectorTest
+	{
+		private UnifiedProgram _model;
+		[SetUp]
+		public void SetUp()
+		{
+            var inputPath = FixtureUtil.GetInputPath("Java", "default", "Collision.java");
+            var code = File.ReadAllText(inputPath, Encoding.Default);
+            _model = JavaFactory.GenerateModel(code);
+		}
+
+		[Test]
+		public void 関数の衝突を検知できる()
+		{
+			
+		}
+
+		[Test]
+		public void 変数の衝突を検知できる()
+		{
+			
+		}
+	}
+}
